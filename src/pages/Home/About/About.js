bf4ce46243534e9b112463e14da6e60ffcc0634c
mdArifulIslam12 <mdarif767778@gmail.com>
2023-01-15 21:50:30 +0600

    update

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -14,7 +14,7 @@ const About = () => {
           <img src="https://travo.iamabdus.com/v1.1/wp-content/uploads/2021/03/business-man-by-skyscraper-1-1.jpg" alt="" className="about-img" />
         </div>
         <div className="col-lg-6 ">
-          <div className="ms-5">
+          <div className="ms-lg-5 about-detail-all">
           <Trending title={'About Us'} subtitle={'a few words'}/>
           <p className="mt-5 about-details">Aliquam erat volutpat. Donec lobortis risus ut nunc ultrices, ut bibendum nibh auctor. Fusce dolor purus, fringilla a felis sed, gravida pellentesque sem. Etiam faucibus massa in fringilla eleifend. In hac habitasse platea dictumst. Duis varius, metus quis sodales mattis, massa dolor consequat urna, quis varius quam mauris id ante. Vestibulum at dictum quam, nec elementum tortor. Duis gravida feugiat ornare. Maecenas a urna in lacus pretium tristique.</p>
           <button className="btn all-button about-button mt-4">
