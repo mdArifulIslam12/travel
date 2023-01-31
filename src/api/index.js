a4061d21543b9bea336191282228947e0521f20e
mdArifulIslam12 <mdarif767778@gmail.com>
2023-01-31 21:20:41 +0600

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const fetcher = axios.create({
-    baseURL: 'http://localhost:5000',
-    // baseURL: 'https://independent-server.onrender.com',
+    baseURL: 'https://independent-server.onrender.com',
   });
 
   export default fetcher
