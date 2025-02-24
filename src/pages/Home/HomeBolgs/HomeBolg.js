53822477e6bccfbc8dbd3e3a10b4801777a6ca40
mdArifulIslam12 <mdarif767778@gmail.com>
2025-02-24 14:07:17 +0600

    update

--- src/pages/Home/HomeBolgs/HomeBolg.js
+++ src/pages/Home/HomeBolgs/HomeBolg.js
@@ -1,11 +1,24 @@
 import React from "react";
+import bolg1 from '../../../images/bolg1.jpg'
+import bolg2 from '../../../images/bolg2.jpg'
+import bolg3 from '../../../images/bolg3.jpg'
+import bolg4 from '../../../images/bolg4.jpg'
+import bolg5 from '../../../images/bolg5.webp'
+
+const imageBolg = {
+  bolg1,
+  bolg2,
+  bolg3,
+  bolg4,
+  bolg5,
+}
 
 const HomeBolg = ({ bolg }) => {
   const { name, img, details, date } = bolg;
   return (
     <div className="col">
       <div class="card homebolg">
-        <img src={img} class="card-img-top" alt="..." />
+        <img src={imageBolg[img]} class="card-img-top" alt="..." />
         <div class="card-body">
           <p className="mt-4 mb-3 homebolg-date">{date}</p>
           <h5 class="card-title ">{name}</h5>
